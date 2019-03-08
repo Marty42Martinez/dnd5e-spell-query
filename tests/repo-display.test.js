dcd93335a57e22416cc875277ea5340609631a17
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-03-08 14:44:52 -0800

    test now dynamic

--- tests/repo-display.test.js
+++ tests/repo-display.test.js
@@ -5,11 +5,11 @@ QUnit.module('Creating LI Templates');
 function makeRepoLI(repository) {
    const html = /*html*/`
    <li>
-        <h2>about-me</h2>
-        <a href="https://github.com/Marty42Martinez/about-me">about-me</a>
-        <p>Description</p>
-        <p>HTML</p>
-        <p>Last updated: 2019-01-29</p>
+        <h2>${repository.name}</h2>
+        <a href="${repository.html_url}">${repository.name}</a>
+        <p>${repository.description ? repository.description : 'Description'}</p>
+        <p>${repository.language}</p>
+        <p>Last updated: ${repository.updated_at.slice(0, 10)}</p>
       </li>`;
    const template = document.createElement('template');
    template.innerHTML = html;
