ddc9f8cd5f3a5a492aa078970059b23e5c4c6041
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-03-08 14:38:48 -0800

    display 3 hard coded repo LIs, first test set up and passing

---
+++ tests/repo-display.test.js
@@ -0,0 +1,42 @@
+const test = QUnit.test;
+
+QUnit.module('Creating LI Templates');
+
+function makeRepoLI(repository) {
+   const html = /*html*/`
+   <li>
+        <h2>about-me</h2>
+        <a href="https://github.com/Marty42Martinez/about-me">about-me</a>
+        <p>Description</p>
+        <p>HTML</p>
+        <p>Last updated: 2019-01-29</p>
+      </li>`;
+   const template = document.createElement('template');
+   template.innerHTML = html;
+   return template.content;
+   
+}
+
+test('create li template when passed repository object', assert => {
+   //Arrange
+   const expected = /*html*/`
+   <li>
+        <h2>about-me</h2>
+        <a href="https://github.com/Marty42Martinez/about-me">about-me</a>
+        <p>Description</p>
+        <p>HTML</p>
+        <p>Last updated: 2019-01-29</p>
+      </li>
+   `;
+   const repository = {
+      name: 'about-me',
+      html_url: 'https://github.com/Marty42Martinez/about-me',
+      description: null,
+      language: 'HTML',
+      updated_at: '2019-01-29T01:01:08Z'
+   };
+   //Act
+   const result = makeRepoLI(repository);
+   //Assert
+   assert.htmlEqual(result, expected);
+});
--- tests/tests.js
+++ tests/tests.js
@@ -1,2 +1,3 @@
 import './html-equal.js';
 // import some tests!
+import './repo-display.test.js';
