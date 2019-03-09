142726f234f44f207e4264d53cb8fb5c4c125ea7
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-03-08 17:24:28 -0800

    update page on hashChange

--- src/build-gh-url.js
+++ src/build-gh-url.js
@@ -1,5 +1,10 @@
-export function searchToBaseURL(options) {
+export function createURL(options) {
    const BASE_URL = `https://api.github.com/users`;
-   const url = `${BASE_URL}/${options.searchTerm}/repos/`;
-   return url;
+   const path = `${BASE_URL}/${options.user}/repos`;
+   const url = new URL(path);
+   url.searchParams.set('sort', 'created');
+   url.searchParams.set('page', options.page);
+   url.searchParams.set('per_page', options.per_page);
+
+   return url.toString();
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import data from '../data/repo-data.js';
 import loadDisplay from './display-component.js';
 import './search-component.js';
+import { readFromHash } from './hash-query.js';
+import { createURL } from './build-gh-url.js';
 
-loadDisplay(data);
+// loadDisplay(data);
 
 window.addEventListener('hashchange', () => {
+   const query = window.location.hash.slice(1);
+   const queryOptions = readFromHash(query);
    
-
+   const url = createURL(queryOptions);
+   fetch(url)
+      .then(response => response.json())
+      .then(repositories => {
+         loadDisplay(repositories);
+      });
 
 });
 
